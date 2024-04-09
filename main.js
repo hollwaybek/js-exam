d2e3fef3863cba0226715d6f5327c011031ad2bb
hollwaybek <hollwayuzb@gmail.com>
2024-04-09 16:36:48 +0500

    done

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+// var Random = Math.round(Math.random() *10);
+// console.log(Random); done
+
+
+
+// var userAge = prompt ('Yoshingizni kiriting'); 
+// if(userAge > 18) {
+//   console.log("Voyaga yetgan yigit");
+// } else {
+//   console.log("Voyaga yetmagan bola");
+// } done
+
+
+
+
+// var Num = "12.510" 
+// console.log(Math.floor(Num)); done
+
+
+
+// var berilgan = "MARS IT SCHOOL";
+// var son = harfSoni(berilgan);
+
+// function harfSoni(str) {
+//     return str.length;
+// }
+
+// alert("MARS IT SCHOOL so'zi " + son + " ta harfdan iborat"); done 
+
+
+
+
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// console.log(harflar[11] + harflar[7] + harflar[16] + harflar[13] + harflar[12] + harflar[17] + harflar[6] + harflar[13]+ harflar[21]); done 
+
+
+// let soz = prompt("Ism");
+// let teskari = "";
+
+// for (let i = soz.length - 1; i >= 0; i--) {
+//     teskari += soz[i];
+// }
+// console.log(teskari); done
+
+
+
+// let son = 5;
+// let boluvchi = 2;
+// let qoldiq = son % boluvchi;
+// alert('Berilgan sonning qoldiq:' + qoldiq); done 
+
+
+
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//     if (sonlar[i] % 2 === 0) {
+//         console.log(sonlar[i]);
+//     }
+// } done   
+
+
+
+// function Mars() {
+//     for (let i = 0; i < 10; i++) {
+//       console.log("MARS IT SCHOOL");
+//     }
+//   }
+
+//   Mars();  done 
